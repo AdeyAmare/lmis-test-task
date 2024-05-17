39c8800ca4f2dae4aa7c7f2a355a2fcf12f52dee
AdeyAmare <amareadey@gmail.com>
2024-05-17 16:49:10 +0300

    little thing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,18 @@
+import { FaCaretDown } from "react-icons/fa"
+
 function Header() {
     return (
-        <div className="flex justify-between px-10 items-center py-2 border-b-2">
+        <div className="flex justify-between px-10 items-center py-3 pt-5 border-b-2">
             <h1 className="font-bold text-xl">Ethiopian National COC</h1>
             <div className="flex items-center gap-x-2">
                 <div className="flex flex-col">
                     <p className="font-semibold">Amanuel Belay</p>
                     <p className="text-sm">Medical Doctor</p>
                 </div>
-                <img src='/assets/profilepic.jpeg' width={50} className="rounded-full" />
+                <img src='/assets/profilepic.jpeg' width={50} className="rounded-full mr-4" />
+                <FaCaretDown />
             </div>
+
         </div>
 
     )
