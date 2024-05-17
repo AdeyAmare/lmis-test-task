3e0b31b21efce9871e9fa462884d0275830bf401
AdeyAmare <amareadey@gmail.com>
2024-05-17 13:35:55 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Dashboard } from "./pages/Dashboard"
+
+
+function App() {
+
+  return (
+    <>
+      {/* Header */}
+
+
+      {/* Side nav */}
+
+      {/* Main page */}
+      <Dashboard />
+    </>
+  )
+}
+
+export default App
